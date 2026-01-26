edd3170538c67d311ef63f0e1b413c2ef8afde6f
arthurantunes94 <arthurantunes0802@gmail.com>
2026-01-26 12:25:18 -0300

    Criacao da delecao de pensamento Criacao da constante de URL_BASE Criacao da mensagem de sem pensamentos cadastrados

--- js/api.js
+++ js/api.js
@@ -1,7 +1,9 @@
+const URL_BASE = 'http://localhost:3000' //Constante para armazenar o endereço base da API
+
 const api = {
     async buscarPensamentos(){
         try {
-            const response = await fetch('http://localhost:3000/pensamentos') //variavel com a resposta da API, onde é necessário passar o endereço 
+            const response = await fetch(`${URL_BASE}/pensamentos`) //variavel com a resposta da API, onde é necessário passar o endereço 
             return await response.json() //Retorno da API
         } catch (error){
             alert('Erro ao buscar pensamentos') //Caso de erro, retorna alerta  
@@ -11,7 +13,7 @@ const api = {
 
     async salvarPensamentos(pensamento){
         try {
-            const response = await fetch('http://localhost:3000/pensamentos', {
+            const response = await fetch(`${URL_BASE}/pensamentos`, {
                method: "POST",
                headers: {
                 "Content-Type": "application/json"
@@ -27,7 +29,7 @@ const api = {
 
     async buscarPensamento(id){
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${id}`) //variavel com a resposta da API, onde é necessário passar o endereço 
+            const response = await fetch(`${URL_BASE}/pensamentos/${id}`) //variavel com a resposta da API, onde é necessário passar o endereço 
             return await response.json() //Retorno da API
         } catch (error){
             alert('Erro ao buscar pensamento') //Caso de erro, retorna alerta  
@@ -37,7 +39,7 @@ const api = {
 
     async editarPensamento(pensamento){
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+            const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
                method: "PUT",
                headers: {
                 "Content-Type": "application/json"
@@ -50,6 +52,17 @@ const api = {
             throw error
         }
     },
+
+    async excluirPensamento(id){
+        try {
+            const response = await fetch(`${URL_BASE}/pensamentos/${id}`, {
+               method: "DELETE",
+            }) //variavel que seta que o método é referente a um DELETE
+        } catch (error){
+            alert('Erro ao excluir pensamento') //Caso de erro, retorna alerta  
+            throw error
+        }
+    }
 }
 
 export default api //Para possibilitar a importação por outras classes
--- js/ui.js
+++ js/ui.js
@@ -2,14 +2,22 @@ import api from './api.js' //importa o scripts api.js
 
 const ui = {
     async renderizarPensamentos(){
+        const listaPensamentos = document.querySelector('#lista-pensamentos')
+        listaPensamentos.innerHTML = '' //Limpa a lista antes de adicionar novos pensamentos
+        const pensamentoVazio = document.getElementById('pensamento-vazio')  
+
         try {
-            const pensamentos = await api.buscarPensamentos() //Criei uma váriavel para guardar todo o response dos pensamentos
- 
-            //forEach para poder varrer todos os pensamentos do json e chama método que cria o elemento na tela
-            pensamentos.forEach(ui.adicionarPensamento)
-        } catch (error) {
-            alert('Erro ao renderizar o pensamento') //Caso não consiga realizar o que está dentro do try, da esse alert
+        const pensamentos = await api.buscarPensamentos() //Criei uma váriavel para guardar todo o response dos pensamentos
+        if (pensamentos.length === 0){
+            pensamentoVazio.style.display = 'flex' //Mostra a mensagem de nenhum pensamento caso o array esteja vazio
+            return
         }
+        //forEach para poder varrer todos os pensamentos do json e chama método que cria o elemento na tela
+        pensamentoVazio.style.display = 'none' //Esconde a mensagem de nenhum pensamento caso o array tenha pensamentos
+        pensamentos.forEach(ui.adicionarPensamento)
+        } catch (error) {
+        alert('Erro ao renderizar o pensamento') //Caso não consiga realizar o que está dentro do try, da esse alert
+        } 
     },
 
     //cria elementos dos pensamentos na tela 
@@ -40,11 +48,22 @@ const ui = {
         btnEditar.classList.add('botao-editar')
         btnEditar.onclick = () => ui.editarPensamento(pensamento.id)
 
+        const btnExcluir = document.createElement('button')
+        btnExcluir.classList.add('botao-excluir')
+        btnExcluir.onclick = () => ui.excluirPensamento(pensamento)
+
         const iconeEditar = document.createElement('img')
         iconeEditar.src = 'assets/imagens/icone-editar.png'
         iconeEditar.alt = 'Editar'
         btnEditar.appendChild(iconeEditar)
+
+        const iconeExcluir = document.createElement('img')
+        iconeExcluir.src = 'assets/imagens/icone-excluir.png'
+        iconeExcluir.alt = 'Excluir'
+        btnExcluir.appendChild(iconeExcluir)
+        
         divIcones.appendChild(btnEditar)
+        divIcones.appendChild(btnExcluir)
 
        
 
@@ -65,6 +84,15 @@ const ui = {
         document.getElementById("pensamento-id").value = pensamento.id
         document.getElementById("pensamento-conteudo").value = pensamento.conteudo
         document.getElementById("pensamento-autoria").value = pensamento.autoria
+    },
+
+    async excluirPensamento(pensamento) {
+        try{
+            await api.excluirPensamento(pensamento.id)
+            ui.renderizarPensamentos()
+        } catch (error) {
+            alert('Erro ao excluir pensamento')
+        }
     }
 } 
 
