9a58f236fbf55db28474cf3c5534488a8e06738f
arthurantunes94 <arthurantunes0802@gmail.com>
2026-01-26 14:07:09 -0300

    Estudo utilizando axios ao invés de fetch

--- js/api.js
+++ js/api.js
@@ -3,8 +3,8 @@ const URL_BASE = 'http://localhost:3000' //Constante para armazenar o endereço 
 const api = {
     async buscarPensamentos(){
         try {
-            const response = await fetch(`${URL_BASE}/pensamentos`) //variavel com a resposta da API, onde é necessário passar o endereço 
-            return await response.json() //Retorno da API
+            const response = await axios.get(`${URL_BASE}/pensamentos`) //variavel com a resposta da API, onde é necessário passar o endereço 
+            return await response.data //Retorno da API
         } catch (error){
             alert('Erro ao buscar pensamentos') //Caso de erro, retorna alerta  
             throw error
@@ -13,14 +13,8 @@ const api = {
 
     async salvarPensamentos(pensamento){
         try {
-            const response = await fetch(`${URL_BASE}/pensamentos`, {
-               method: "POST",
-               headers: {
-                "Content-Type": "application/json"
-               },
-               body: JSON.stringify(pensamento)
-            }) //variavel que seta que o método é referente a um POST
-            return await response.json() //Retorno da API
+            const response = await axios.post(`${URL_BASE}/pensamentos`, pensamento) //variavel que seta que o método é referente a um POST
+            return await response.data //Retorno da API
         } catch (error){
             alert('Erro ao salvar pensamentos') //Caso de erro, retorna alerta  
             throw error
@@ -29,8 +23,8 @@ const api = {
 
     async buscarPensamento(id){
         try {
-            const response = await fetch(`${URL_BASE}/pensamentos/${id}`) //variavel com a resposta da API, onde é necessário passar o endereço 
-            return await response.json() //Retorno da API
+            const response = await axios.get(`${URL_BASE}/pensamentos/${id}`) //variavel com a resposta da API, onde é necessário passar o endereço 
+            return await response.data //Retorno da API
         } catch (error){
             alert('Erro ao buscar pensamento') //Caso de erro, retorna alerta  
             throw error
@@ -39,14 +33,8 @@ const api = {
 
     async editarPensamento(pensamento){
         try {
-            const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
-               method: "PUT",
-               headers: {
-                "Content-Type": "application/json"
-               },
-               body: JSON.stringify(pensamento)
-            }) //variavel que seta que o método é referente a um PUT
-            return await response.json() //Retorno da API
+            const response = await axios.put(`${URL_BASE}/pensamentos/${pensamento.id}`, pensamento) //variavel que seta que o método é referente a um PUT
+            return await response.data //Retorno da API
         } catch (error){
             alert('Erro ao editar pensamento') //Caso de erro, retorna alerta  
             throw error
@@ -55,14 +43,13 @@ const api = {
 
     async excluirPensamento(id){
         try {
-            const response = await fetch(`${URL_BASE}/pensamentos/${id}`, {
-               method: "DELETE",
-            }) //variavel que seta que o método é referente a um DELETE
+            const response = await axios.delete(`${URL_BASE}/pensamentos/${id}`) //variavel que seta que o método é referente a um DELETE
+            return await response.data //Retorno da API
         } catch (error){
-            alert('Erro ao excluir pensamento') //Caso de erro, retorna alerta  
+            alert('Erro ao salvar pensamentos') //Caso de erro, retorna alerta  
             throw error
         }
-    }
+    },
 }
 
 export default api //Para possibilitar a importação por outras classes
